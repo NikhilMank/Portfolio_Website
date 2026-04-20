46a5d86534fe95142e54d2495b3b742a395b2f75
NikhilMank <mankalinikhil9@gmail.com>
2026-04-20 20:17:36 +0200

    corrected TextSplitter import to newer version

--- backend/scripts/build_index.py
+++ backend/scripts/build_index.py
@@ -3,7 +3,7 @@ import boto3
 from pathlib import Path
 from dotenv import load_dotenv
 from langchain_community.document_loaders import DirectoryLoader, UnstructuredMarkdownLoader
-from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_aws import BedrockEmbeddings
 from langchain_community.vectorstores import FAISS
 
